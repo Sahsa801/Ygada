0fd8c5c8a2481ca75e2765234f28954c1620d64d
Sahsa801 <maksimsergijovic438@gmail.com>
2025-03-19 17:57:11 +0200

    19.03.2025

--- may.py
+++ may.py
@@ -1,8 +1,31 @@
-max_number = float(input("Введіть число 1: "))
+while True:
+    try:
+        num1 = float(input("Введіть перше число: "))
+        operator = input("Введіть оператор (+, -, *, /): ")
+        num2 = float(input("Введіть друге число: "))
 
-for i in range(9):
-    num = float(input(f"Введіть число {i+2}: "))
-    if num > max_number:
-        max_number = num
+        if operator == '+':
+            result = num1 + num2
+        elif operator == '-':
+            result = num1 - num2
+        elif operator == '*':
+            result = num1 * num2
+        elif operator == '/':
+            if num2 == 0:
+                print("Помилка: ділення на нуль!")
+                continue
+            result = num1 / num2
+        else:
+            print("Невірний оператор!")
+            continue
 
-print(f"Найбільше введене число: {max_number}")
+        print(f"Результат: {result}")
+
+    except ValueError:
+        print("Помилка: введіть коректні числа!")
+        continue
+
+    choice = input("Бажаєте продовжити? (так/ні): ").strip().lower()
+    if choice != 'так':
+        print("Калькулятор завершив роботу.")
+        break
