b47e28e069f9a96ed4feabac14b5df4ec4f43ef4
Sahsa801 <maksimsergijovic438@gmail.com>
2025-03-22 18:36:11 +0200

    22.03.2025

--- may.py
+++ may.py
@@ -1,31 +1,8 @@
-while True:
-    try:
-        num1 = float(input("Введіть перше число: "))
-        operator = input("Введіть оператор (+, -, *, /): ")
-        num2 = float(input("Введіть друге число: "))
+import random
 
-        if operator == '+':
-            result = num1 + num2
-        elif operator == '-':
-            result = num1 - num2
-        elif operator == '*':
-            result = num1 * num2
-        elif operator == '/':
-            if num2 == 0:
-                print("Помилка: ділення на нуль!")
-                continue
-            result = num1 / num2
-        else:
-            print("Невірний оператор!")
-            continue
+def generate_name():
+    first_names = ["Олександр", "Марія", "Іван", "Ольга", "Петро", "Наталія"]
+    last_names = ["Пригорнёв", "Мельниченко", "Шевченко", "Бондаренко", "Товпашко", "Кравець"]
+    return f"{random.choice(first_names)} {random.choice(last_names)}"
 
-        print(f"Результат: {result}")
-
-    except ValueError:
-        print("Помилка: введіть коректні числа!")
-        continue
-
-    choice = input("Бажаєте продовжити? (так/ні): ").strip().lower()
-    if choice != 'так':
-        print("Калькулятор завершив роботу.")
-        break
+print(f"Згенероване ім'я: {generate_name()}")
